88155186fed84585d6d2f93d3905d8c51c74bc94
Nikita Brezitskii <nbrezitskii@powerofgeeks.com>
2022-12-19 16:14:10 +0300

    db manipulations

--- cmd/app.go
+++ cmd/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	todo "todo-app"
 	"todo-app/pkg/handler"
@@ -9,12 +10,23 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error reading configs, %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error running http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
